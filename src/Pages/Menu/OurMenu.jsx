badb9fa43de7cd9dfda065bee14ed3e71e52938a
Mohsin <mohsin@localhost.localdomain>
2024-03-24 16:49:21 +0600

    parallax added

--- src/Pages/Menu/OurMenu.jsx
+++ src/Pages/Menu/OurMenu.jsx
@@ -1,12 +1,13 @@
 import { Helmet } from "react-helmet-async";
-
+import menuBanner from '../../assets/menu/banner3.jpg'
+import CoverImg from "../Shared/CoverImg/CoverImg";
 const OurMenu=()=>{
     return(
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-             <p> HELLO I Am OurMenu</p>
+             <CoverImg img={menuBanner} title = "Our Menu" />
         </div>
     )}
 export default OurMenu;
---
+++ src/Pages/Shared/CoverImg/CoverImg.jsx
@@ -0,0 +1,24 @@
+import { Parallax } from "react-parallax";
+
+const CoverImg = ({ img, title }) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={img}
+      bgImageAlt="the dog"
+      strength={-200}
+    >
+      <div className="hero min-h-[500px]">
+        <div className="hero-content text-center text-neutral-content bg-opacity-60 bg-gray-900 md:w-5/12 h-[250px]">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-6xl font-cinzel font-bold">{title}</h1>
+            <p className="mb-5 font-cinzel text-2xl font-semibold">
+              Would You Like to Try A Dish?
+            </p>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+export default CoverImg;
