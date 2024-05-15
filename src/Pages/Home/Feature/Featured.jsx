3bb030f9ec2b25eccf034ad814523ed1a1c9da8a
Mohsin <mohsin@localhost.localdomain>
2024-05-15 12:34:57 +0600

    paralax image fixed

--- src/Pages/Home/Feature/Featured.jsx
+++ src/Pages/Home/Feature/Featured.jsx
@@ -15,7 +15,7 @@ const Featured=()=>{
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita hic dolorem, iusto vel suscipit nam excepturi debitis magnam nostrum! Ut eum dignissimos culpa doloremque eligendi consectetur blanditiis laboriosam fugiat ea quia similique quam nisi reprehenderit numquam magnam nemo vitae cupiditate, atque maiores dicta minus pariatur. Perspiciatis nobis vero quas?</p>
-                    <Link><button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button></Link>
+                    <Link to='/shop/salad'><button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button></Link>
                     
                 </div>
                 </div>
